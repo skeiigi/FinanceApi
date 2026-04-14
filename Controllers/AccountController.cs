15c90fe6d4156eec67e7640eea01b25f7f590094
neithiri <popokakapo76xxx@gmail.com>
2026-04-15 02:37:36 +0700

    register

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceApi.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterModel model)
+        {
+            var user = new IdentityUser
+            {
+                UserName = model.Email,
+                Email = model.Email,
+                PhoneNumber = model.PhoneNumber // Вот здесь происходит магия записи в БД
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                return Ok(new { Message = "User registered successfully" });
+            }
+
+            return BadRequest(result.Errors);
+        }
+    }
+}
---
+++ Models/RegisterModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FinanceApi.Models
+{
+    public class RegisterModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
